0a7f929b222081b027b010e0ff0ead57ba37eca5
djlabat <djlabat@gmail.com>
2021-01-06 07:20:12 +0100

    Language icon align-right

---
+++ eng/Counting_text_characters.js
@@ -0,0 +1,46 @@
+function characterCounting() {
+  document.getElementById('output').textContent = ""
+  let txt = document.getElementById('txt').value
+  let ts = document.getElementById('ts').value // unet tekst
+  let arr = txt.split('').sort().join('').match(/(.)\1*/gm)
+  
+  
+  for (let item of arr) {
+    document.getElementById('output').textContent = 'nema!'
+    if (ts === item[0]) {
+      document.getElementById('output').textContent = item.length   
+      break 
+    }  
+  }
+}
+
+function characterAmountList() {
+  document.getElementById('tab').textContent = ""
+  let txt = document.getElementById('txt').value
+  let arr = txt.split('').sort().join('').match(/(.)\1*/gm).reverse()
+
+  var table = document.getElementById("tab");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = `Slovo`;
+  cell2.innerHTML = `Kolicina`;
+
+  for (let item of arr) {
+      table = document.getElementById("tab");
+      row = table.insertRow(0);
+      cell1 = row.insertCell(0);
+      cell2 = row.insertCell(1);
+      cell1.innerHTML = `${item[0]}`;
+      cell2.innerHTML = `${item.length}`;
+  }
+
+  table = document.getElementById("tab");
+  row = table.insertRow(0);
+  cell1 = row.insertCell(0);
+  cell2 = row.insertCell(1);
+  cell1.innerHTML = `Slovo`;
+  cell2.innerHTML = `Kolicina`;
+}
+
+
